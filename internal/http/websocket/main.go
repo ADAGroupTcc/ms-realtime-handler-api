b2ec1364e3298f43c0f9bcfcd92783e0b394889c
Raphael Machado <raphael.machado@picpay.com>
2024-05-21 22:33:40 -0300

    config websocket handler

--- internal/http/websocket/main.go
+++ internal/http/websocket/main.go
@@ -1,13 +1,11 @@
 package websocket
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
-
 	"github.com/PicPay/lib-go-instrumentation/interfaces"
+	logger "github.com/PicPay/lib-go-logger/v2"
+	"github.com/PicPay/ms-chatpicpay-websocket-handler-api/internal/http/helpers"
 	"github.com/PicPay/ms-chatpicpay-websocket-handler-api/pkg/pubsubconnector"
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
@@ -17,59 +15,67 @@ var (
 		WriteBufferSize: 1024,
 	}
 
-	users = map[string]*websocket.Conn{}
+	activeConnections = helpers.NewActiveConnections()
 )
 
 type websocketHandler struct {
 	broker     *pubsubconnector.PubSubBroker
 	instrument interfaces.Instrument
+	log        *logger.Logger
 }
 
-func NewHandler(broker *pubsubconnector.PubSubBroker, instrument interfaces.Instrument) *websocketHandler {
+func NewHandler(broker *pubsubconnector.PubSubBroker, instrument interfaces.Instrument, log *logger.Logger) *websocketHandler {
 	return &websocketHandler{
 		broker:     broker,
 		instrument: instrument,
+		log:        log,
 	}
 }
 
-func (h *websocketHandler) WebsocketServer() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println("Failed to upgrade connection:", err)
-			return
-		}
-		defer conn.Close()
-
-		fmt.Println("Conectado: ", r.Header.Get("user_id"))
-
-		user_id := r.Header.Get("user_id")
-		users[user_id] = conn
-
-		fmt.Println("Conexões: ", users)
-		ctx := r.Context()
-
-		go func() {
-			for {
-				_, msg, err := conn.ReadMessage()
-				if err != nil {
-					if strings.Contains(err.Error(), "websocket: close 1000") {
-						fmt.Println("Conexão encerrada: ", r.Header.Get("user_id"))
-						delete(users, r.Header.Get("user_id"))
-						fmt.Println("Conexões: ", users)
-						return
-					}
-					log.Println("Failed to read message from WebSocket client:", err)
-					return
-				}
-
-				if err := h.broker.Publisher.Publisher(ctx, "event_publisher", string(msg)); err != nil {
-					log.Println("Failed to publish message to Redis PubSub:", err)
-					return
-				}
-			}
-		}()
-
-		select {}
+func (h *websocketHandler) WebsocketServer(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		h.log.Error("websocket_handler: failed to upgrade connection", err)
+		return
 	}
+	defer conn.Close()
+
+	ctx := c.Request.Context()
+
+	userId := c.Request.Header.Get("user_id")
+	h.log.Debugf("websocket_handler: user_id: %s connected", userId)
+
+	activeConnections.SetConn(userId, conn)
+	h.log.Debugf("websocket_handler: number of active connections: %d", activeConnections.ConnectionSize())
+
+	go func() {
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure) {
+					h.log.Error("websocket_handler: connection closed unexpectedly", err)
+					activeConnections.DeleteConn(userId)
+					return
+				}
+
+				if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+					h.log.Debug("websocket_handler: connection closed")
+					activeConnections.DeleteConn(userId)
+
+					return
+				}
+
+				activeConnections.DeleteConn(userId)
+				h.log.Error("websocket_handler: failed to read message from webSocket client", err)
+				return
+			}
+
+			if err := h.broker.Publisher.Publisher(ctx, "event_publisher", string(msg)); err != nil {
+				h.log.Error("websocket_handler: failed to publish message to pubsub broker", err)
+				return
+			}
+		}
+	}()
+
+	select {}
 }
